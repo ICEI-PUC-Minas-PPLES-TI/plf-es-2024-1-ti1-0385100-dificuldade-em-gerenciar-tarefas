60e9f0f378c17a95fff14a3b0298704394d7dadc
Henrique Gouvea <henriquedelacamp@gmail.com>
2024-06-11 20:25:50 -0300

    Criacao-ranking

---
+++ codigo/assets/js/ranking.js
@@ -0,0 +1,17 @@
+console.log("hello")
+
+const friends = document.getElementsByClassName("friend")
+
+
+for (let index = 0; index < friends.length; index++) {
+    const level = document.createElement("h1")
+    level.innerText = "level"
+
+    const pontos = document.createElement("h1")
+    pontos.innerText = "80pts"
+
+    friends[index].appendChild(level)
+    friends[index].appendChild(pontos)
+
+    
+}
